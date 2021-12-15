f9cadbaf88399d2c5434c4c9f4dd141d3c6c2a77
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2021-12-15 16:54:24 +0200

    .

--- IP_exercise/14dec_group1/main.cpp
+++ IP_exercise/14dec_group1/main.cpp
@@ -10,12 +10,39 @@ bool equal(const int *begin_1, const int *end_1, const int *begin_2, const int *
     return i == end_1 - begin_1;
 }
 
+const int *search(const int *begin, const int *end, const int *s_begin, const int *s_end)
+{
+    const int *ptr = begin;
+    const int *srch = nullptr;
+    unsigned i = 0, size = s_end - s_begin;
+    do
+    {
+        while (ptr != end && *ptr != *s_begin)
+            ++ptr;
+        srch = ptr;
+        i = 0;
+        while (i < size && srch != end && *srch == *(ptr + i))
+        {
+            ++i;
+            ++srch;
+        }
+        ++ptr;
+    } while (i != size && ptr < end - size);
+    if (i == size)
+        return ptr - 1;
+    return nullptr;
+}
+
 int main()
 {
     int a[] = {1, 2, 1, 3};
-    int b[] = {2, 1};
+    int b[] = {1,3};
     std::cout << equal(a, a + 2, b, b + 2) << '\n';
-    std::cout << equal(a + 1, a + 3, b, b + 2) << '\n';
-
+    std::cout << equal(a + 2, a + 4, b, b + 2) << '\n';
+    const int *search_res = search(a, a + 4, b, b + 2);
+    if (search_res)
+        std::cout << search_res - a << '\n';
+    else
+        std::cout << "Not found.\n";
     return 0;
 }
