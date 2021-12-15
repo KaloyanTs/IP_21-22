182df0669dd992f71bc7fe409431d5a473acd57b
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2021-12-15 17:29:32 +0200

    search

--- IP_exercise/14dec_group1/main.cpp
+++ IP_exercise/14dec_group1/main.cpp
@@ -4,10 +4,12 @@ bool equal(const int *begin_1, const int *end_1, const int *begin_2, const int *
 {
     if (end_1 - begin_1 != end_2 - begin_2)
         return false;
-    unsigned i = 0;
-    while (i < end_1 - begin_1 && *(begin_1 + i) == *(begin_2 + i))
-        ++i;
-    return i == end_1 - begin_1;
+    while (begin_2 != end_2 && *begin_1 == *begin_2)
+    {
+        ++begin_1;
+        ++begin_2;
+    }
+    return begin_2 == end_2;
 }
 
 const int *search(const int *begin, const int *end, const int *s_begin, const int *s_end)
@@ -15,31 +17,22 @@ const int *search(const int *begin, const int *end, const int *s_begin, const in
     const int *ptr = begin;
     const int *srch = nullptr;
     unsigned i = 0, size = s_end - s_begin;
+    const int *max = end - size + 1;
     do
     {
-        while (ptr != end && *ptr != *s_begin)
+        while (ptr != max && *ptr != *s_begin)
             ++ptr;
-        srch = ptr;
-        i = 0;
-        while (i < size && srch != end && *srch == *(ptr + i))
-        {
-            ++i;
-            ++srch;
-        }
-        ++ptr;
-    } while (i != size && ptr < end - size);
-    if (i == size)
-        return ptr - 1;
-    return nullptr;
+    } while (!equal(ptr, ptr + size, s_begin, s_end) && ++ptr < max);
+    return (ptr < max ? ptr : nullptr);
 }
 
 int main()
 {
-    int a[] = {1, 2, 1, 3};
-    int b[] = {1,3};
-    std::cout << equal(a, a + 2, b, b + 2) << '\n';
+    int a[] = {1, 2, 1, 3, 7, 2, 1, 9};
+    int b[] = {1, 3};
     std::cout << equal(a + 2, a + 4, b, b + 2) << '\n';
-    const int *search_res = search(a, a + 4, b, b + 2);
+    std::cout << equal(a + 6, a + 8, b, b + 2) << '\n';
+    const int *search_res = search(a, a + 8, b, b + 2);
     if (search_res)
         std::cout << search_res - a << '\n';
     else
