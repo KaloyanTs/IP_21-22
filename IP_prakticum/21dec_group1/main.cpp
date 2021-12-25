831cdf820c4e8e42d0dade5aa1bde2cc38b1df76
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2021-12-25 12:57:32 +0200

    Task3

--- IP_prakticum/21dec_group1/main.cpp
+++ IP_prakticum/21dec_group1/main.cpp
@@ -72,10 +72,67 @@ void Task2()
     std::cout << maxWord << '\n';
 }
 
+void swap(char &c1, char &c2)
+{
+    char tmp = c1;
+    c1 = c2;
+    c2 = tmp;
+}
+
+char *selSort(char *arr)
+{
+    char *arrCpy = arr;
+    char *parr = arr;
+    char *maxEl = arr;
+    while (*arr)
+    {
+        parr = arr + 1;
+        maxEl = arr;
+        while (*parr)
+        {
+            if (*parr > *maxEl)
+                maxEl = parr;
+            ++parr;
+        }
+        swap(*arr++, *maxEl);
+    }
+    return arrCpy;
+}
+
+void getCommon(const char *str1, const char *str2, char *res)
+{
+    while (*str1 && *str2)
+    {
+        if (*str1 == *str2)
+        {
+            *res++ = *str1;
+            ++str1;
+            ++str2;
+        }
+        else if (*str1 > *str2)
+            ++str1;
+        else if (*str1 < *str2)
+            ++str2;
+    }
+    *res = '\0';
+}
+
+void Task3()
+{
+    const unsigned MAX = 20;
+    char num1[MAX], num2[MAX], common[MAX];
+    std::cin.getline(num1, MAX);
+    std::cin.getline(num2, MAX);
+    selSort(num1);
+    selSort(num2);
+    getCommon(num1, num2, common);
+    std::cout << common << '\n';
+}
+
 int main()
 {
-    //Task1()
-    //Task2()
-    
+    // Task1()
+    // Task2()
+    Task3();
     return 0;
 }
