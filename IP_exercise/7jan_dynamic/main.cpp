c404c09d9d6d0e0b4dc06f130fde6a4a65a58ce2
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-01-07 15:24:55 +0200

    small upgrades

--- IP_exercise/7jan_dynamic/main.cpp
+++ IP_exercise/7jan_dynamic/main.cpp
@@ -1,42 +1,62 @@
 #include <iostream>
 #include <cstring>
 
+void addWordToList(char *list, unsigned &l, const char *word)
+{
+    if (l)
+        list[l++] = ' ';
+    list[l] = '\0';
+    while (*word)
+        list[l++] = *word++;
+    list[l++] = ',';
+    list[l] = '\0';
+}
+
 char *repeatingWords(const char *text)
 {
     unsigned l = 0;
-    char *textCpy = new (std::nothrow) char[strlen(text) + 1];
-    if (!textCpy)
+    char *word = new (std::nothrow) char[strlen(text) + 1];
+    if (!word)
         return nullptr;
-    strcpy(textCpy, text);
-    text = textCpy;
+    strcpy(word, text);
+    //за да можем да работим с подадения низ го копираме в нов
+    text = word;
     char *buf = new (std::nothrow) char[strlen(text) / 2 + 1];
+    //в най-лошия случай всяка дума ще я има точно два пъти и ще се нуждаем от <= толкова място
     *buf = '\0';
     if (!buf)
         return nullptr;
-    char *textPtr = textCpy;
+    char *textPtr = word;
     while (*textPtr)
     {
         while (*textPtr && *textPtr != ',')
             ++textPtr;
+        //търсим следващата дума
         if (*textPtr)
         {
             *textPtr = '\0';
-            if (strstr(textPtr + 1, textCpy) && !strstr(buf, textCpy))
+            if (strstr(textPtr + 1, word) && !strstr(buf, word))
+            //има я напред и не е намирана досега
             {
+                addWordToList(buf, l, word);
+                /*
                 if (l)
                     buf[l++] = ' ';
                 buf[l] = '\0';
-                strcat(buf, textCpy);
-                l += strlen(textCpy);
+                strcat(buf, word);          //  addWordToList
+                l += strlen(word);          //               ни спестява малко време спрямо тези 2 реда?
                 buf[l++] = ',';
                 buf[l] = '\0';
+                */
             }
-            textCpy = textPtr += 2;
+            word = textPtr += 2;
+            //прескачаме ", "
         }
     }
     if (!l)
         ++l;
     buf[l - 1] = '\0';
+    //на тази позиция има ',', която не ни трябва повече
     char *res = new (std::nothrow) char[l];
     if (res)
         strcpy(res, buf);
@@ -45,7 +65,7 @@ char *repeatingWords(const char *text)
     return res;
 }
 
-int main()
+void repeatingWordsListTask()
 {
     const unsigned MAX = 128;
     char buf[MAX] = "cat, dog, cat, I, am, qk, professor, qk, hello";
@@ -58,5 +78,10 @@ int main()
         std::cout << bufRes << '\n';
         delete[] bufRes;
     }
+}
+
+int main()
+{
+    repeatingWordsListTask();
     return 0;
 }
