083bfccff9791d91d47f43a14ef49fa3eb937c85
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-01-22 13:04:44 +0200

    Task2 quadratic equations

--- IP_prakticum/22jan/main.cpp
+++ IP_prakticum/22jan/main.cpp
@@ -62,7 +62,7 @@ unsigned countThisSum(int *arr, unsigned N, unsigned s)
     return res;
 }
 
-int **allocMatrixSum(int *arr, unsigned N, unsigned n, unsigned m)
+int **allocMatrixSum(int *arr, unsigned N, unsigned n)
 {
     int **M = new (std::nothrow) int *[n + 1];
     if (!M)
@@ -70,6 +70,7 @@ int **allocMatrixSum(int *arr, unsigned N, unsigned n, unsigned m)
     for (unsigned i = 0; i <= n; ++i)
     {
         M[i] = new (std::nothrow) int[countThisSum(arr, N, i) + 1];
+        // M[i] = new (std::nothrow) int[<всички числа> + 1];
         if (!M[i])
         {
             deallocMatrix(M, i);
@@ -109,14 +110,14 @@ void Task1b()
     if (!arr)
     {
         std::cout << "Not enough memory!\n";
-        return 0;
+        return;
     }
     unsigned maxS = maxSum(arr, N);
-    int **sums = allocMatrixSum(arr, N, maxS, N);
+    int **sums = allocMatrixSum(arr, N, maxS);
     if (!sums)
     {
         std::cout << "Not enough memory!\n";
-        return 0;
+        return;
     }
     fillSums(sums, maxS, arr, N);
     printSums(sums, maxS);
@@ -124,9 +125,101 @@ void Task1b()
     deallocMatrix(sums, maxS + 1);
 }
 
+void deallocEquations(int **&eq, unsigned N)
+{
+    for (unsigned i = 0; i < N; ++i)
+        delete[] eq[i];
+    delete[] eq;
+    eq = nullptr;
+}
+
+int **allocEquations(unsigned N)
+{
+    int **eq = new (std::nothrow) int *[N];
+    if (!eq)
+        return nullptr;
+    for (unsigned i = 0; i < N; ++i)
+    {
+        eq[i] = new (std::nothrow) int[4];
+        // eq[i]=0 when no root found
+        // eq[i]!=0 when found a root
+        if (!eq[i])
+        {
+            deallocEquations(eq, i);
+            return nullptr;
+        }
+        eq[i][0] = 0;
+        std::cin >> eq[i][1] >> eq[i][2] >> eq[i][3];
+    }
+    return eq;
+}
+
+void calc(int *eq, int arg)
+{
+    eq[0] = (eq[1] * arg * arg + eq[2] * arg + eq[3] == 0);
+}
+
+void tryForRoots(int **eq, unsigned N, int *arg, unsigned K)
+{
+    for (unsigned i = 0; i < N; ++i)
+    {
+        unsigned j = 0;
+        while (j < K && !eq[i][0])
+            calc(eq[i], arg[j++]);
+    }
+}
+
+void printEquations(int **eq, unsigned N)
+{
+    for (unsigned i = 0; i < N; ++i)
+        if (eq[i][0])
+        {
+            for (unsigned j = 1; j <= 3; ++j)
+                std::cout << eq[i][j] << ' ';
+            std::cout << '\n';
+        }
+}
+
+int *readArguments(unsigned K)
+{
+    int *arg = new (std::nothrow) int[K];
+    if (!arg)
+        return nullptr;
+    for (unsigned i = 0; i < K; ++i)
+        std::cin >> arg[i];
+    return arg;
+}
+
+void Task2()
+{
+    unsigned N;
+    std::cin >> N;
+    int **eq = allocEquations(N);
+    if (!eq)
+    {
+        std::cout << "Not enough memory!\n";
+        return;
+    }
+    unsigned K;
+    std::cin >> K;
+    int *arguments = readArguments(K);
+    if (!arguments)
+    {
+        deallocEquations(eq, N);
+        std::cout << "Not enough memory!\n";
+        return;
+    }
+    tryForRoots(eq, N, arguments, K);
+    printEquations(eq, N);
+
+    deallocEquations(eq, N);
+    delete[] arguments;
+}
+
 int main()
 {
     // Task1a();
-    Task1b();
+    // Task1b();
+    Task2();
     return 0;
 }
